0a98827d05f63ba2ad55fb27cd90533c118b1925
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-05-02 18:42:24 -0400

    adding product routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,6 +7,24 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [
+        {
+          model: Category,
+          as: "category",
+        },
+        {
+          model: Tag,
+          through: ProductTag,
+          as: "tags",
+        },
+      ],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
@@ -90,7 +108,20 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  try {
+      const productData = await Product.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (!productData) {
+        res.status(404).json({ message: "No product found with this id!" });
+        return;
+      }
+      res.status(200).json(productData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
 });
 
 module.exports = router;
